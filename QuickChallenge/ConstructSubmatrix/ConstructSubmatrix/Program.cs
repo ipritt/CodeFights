f50e3ac4cba15df3f7d3a50a684535dfdc452ca0
Blargg <ianpritt@yahoo.com>
2018-06-26 15:59:59 -0400

    QuickChallenges

---
+++ QuickChallenge/ConstructSubmatrix/ConstructSubmatrix/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Given a matrix(i.e.an array of arrays), find its submatrix obtained by deleting the specified rows and columns.
+
+//Example
+
+//For
+
+//matrix = [[1, 0, 0, 2], 
+//          [0, 5, 0, 1], 
+//          [0, 0, 3, 5]]
+//rowsToDelete = [1] and columnsToDelete = [0, 2], the output should be
+
+//constructSubmatrix(matrix, rowsToDelete, columnsToDelete) = [[0, 2],
+//                                                             [0, 5]]
+//Input/Output
+
+//[execution time limit] 3 seconds(cs)
+
+//[input]
+//array.array.integer matrix
+
+//2-dimensional array of integers.
+
+//Guaranteed constraints:
+//1 ≤ matrix.length ≤ 5,
+//1 ≤ matrix[0].length ≤ 5,
+//0 ≤ matrix[i][j] ≤ 15.
+
+//[input] array.integer rowsToDelete
+
+//Array of indices (0-based) of rows to be deleted.
+
+//Guaranteed constraints:
+//0 ≤ rowsToDelete.length ≤ 2,
+//0 ≤ rowsToDelete[i] < matrix.length.
+
+//[input] array.integer columnsToDelete
+
+//Array of indices (0-based) of columns to be deleted.
+
+//Guaranteed constraints:
+//0 ≤ columnsToDelete.length ≤ 2,
+//0 ≤ columnsToDelete[i] < matrix[0].length.
+
+//[output] array.array.integer
+
+namespace ConstructSubmatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsToDelete = { 1 };
+            int[] columnsToDelete = { 0, 2 };
+            int[][] matrix = 
+            {
+                new int[] { 1, 0, 0, 2 },
+                new int[] { 0, 5, 0, 1 },
+                new int[] { 0, 0, 3, 5 }
+            };
+            int[][] result = ConstructSubmatrix(matrix, rowsToDelete, columnsToDelete);
+            for(int i = 0; i < result.Length; i++)
+            {
+                for(int j = 0; j < result[0].Length; j++)
+                {
+                    Console.Write(result[i][j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        static int[][] ConstructSubmatrix(int[][] matrix, int[] rowsToDelete, int[] columnsToDelete)
+        {
+            int listCount = 0;
+            List<int> intList = new List<int>();
+            int[][] result = new int[matrix.Length - rowsToDelete.Length][];
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = new int[matrix[0].Length - columnsToDelete.Length];
+            }
+            for(int i = 0; i < matrix.Length; i++)
+            {
+                for(int j = 0; j < matrix[0].Length; j++)
+                {
+                    if(!(rowsToDelete.Contains(i) || columnsToDelete.Contains(j)))
+                    {
+                        intList.Add(matrix[i][j]);
+                    }
+                }
+            }
+            for(int i = 0; i < result.Length; i++)
+            {
+                for(int j = 0; j < result[0].Length; j++)
+                {
+                    result[i][j] = intList[listCount];
+                    listCount++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ QuickChallenge/Zigzag/Zigzag/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+//A sequence of integers is called a zigzag sequence if each of its elements is either strictly less than all its neighbors or strictly greater than all its neighbors.For example, the sequence 4 2 3 1 5 3 is a zigzag, but 7 3 5 5 2 and 3 8 6 4 5 aren't. Sequence of length 1 is also a zigzag.
+
+//For a given array of integers return the length of its longest contiguous sub-array that is a zigzag sequence.
+
+//Example
+
+//For a = [9, 8, 8, 5, 3, 5, 3, 2, 8, 6], the output should be
+//zigzag(a) = 4.
+
+//The longest zigzag sub-arrays are [5, 3, 5, 3] and[3, 2, 8, 6] and they both have length 4.
+
+//For a = [4, 4], the output should be
+//zigzag(a) = 1.
+
+//The longest zigzag sub-array is [4] - it has only one element, which is strictly greater than all its neighbors (there are none of them).
+
+//Input/Output
+
+//[execution time limit] 3 seconds(cs)
+
+//[input]
+//array.integer a
+
+//Guaranteed constraints:
+//2 ≤ a.length ≤ 25,
+//0 ≤ a[i] ≤ 100.
+
+//[output]
+//integer
+
+//[C#] Syntax Tips
+
+namespace Zigzag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = { 0, 0, 0, 0, 1, 0, 0, 0, 1 };
+            //9, 8, 8, 5, 3, 5, 3, 2, 8, 6 exp 4
+            //2, 1, 4, 4, 1, 4, 4, 1, 2, 0, 1, 0, 0, 3, 1, 3, 4, 1, 3, 4 exp 6
+            int output = Zigzag(sequence);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+
+        static int Zigzag(int[] a)
+        {
+            int result = 0;
+            int previous = 0;
+            int next = 0;
+            List<int> subList = new List<int>();
+            if (a.Length == 1 || a.Length == 2)
+            {
+                return 1;
+            }
+            for (int i = 1; i < a.Length - 1; i++)
+            {
+                previous = a[i - 1];
+                next = a[i + 1];
+                if((a[i] < previous && a[i] < next) || (a[i] > previous && a[i] > next))
+                {
+                    if(subList.Count == 0)
+                    {
+                        subList.Add(previous);
+                        subList.Add(a[i]);
+                    }
+                    else
+                    {
+                        subList.Add(a[i]);
+                    }
+                    if(result < subList.Count + 1)
+                    {
+                        result = subList.Count + 1;
+                    }
+                }
+                else
+                {
+                    subList.Clear();
+                }
+            }
+            return result;
+        }
+    }
+}
