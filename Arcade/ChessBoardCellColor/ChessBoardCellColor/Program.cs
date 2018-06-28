42df64f90e56d23119bf78808a0043447825ec86
Blargg <ianpritt@yahoo.com>
2018-06-28 11:35:58 -0400

    ChessBoardCellColorAdjusted

--- Arcade/ChessBoardCellColor/ChessBoardCellColor/Program.cs
+++ Arcade/ChessBoardCellColor/ChessBoardCellColor/Program.cs
@@ -1,8 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+//Given two cells on the standard chess board, determine whether they have the same color or not.
+
+//Example
+
+//For cell1 = "A1" and cell2 = "C3", the output should be
+//chessBoardCellColor(cell1, cell2) = true.
+
+
+
+//For cell1 = "A1" and cell2 = "H3", the output should be
+//chessBoardCellColor(cell1, cell2) = false.
+
+
+
+//Input / Output
+
+//[execution time limit] 3 seconds (cs)
+
+//[input] string cell1
+
+//[input] string cell2
+
+//[output] boolean
+
+//true if both cells have the same color, false otherwise.
 
 namespace ChessBoardCellColor
 {
@@ -54,15 +77,7 @@ namespace ChessBoardCellColor
 
         private static string Flip(string color)
         {
-            if (color == "black")
-            {
-                color = "white";
-            }
-            else
-            {
-                color = "black";
-            }
-            return color;
+            return (color == "black") ? "white" : "black";
         }
     }
 }
